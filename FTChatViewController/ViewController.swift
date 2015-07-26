9ca1b67c70f4af943c827ccff79f0d098a0d0aed
fthomasmorel <fthomasm@insa-rennes.fr>
2015-07-26 10:53:10 +0200

    Initial Commit

---
+++ FTChatViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FTChatViewController
+//
+//  Created by Florent TM on 26/07/2015.
+//  Copyright © 2015 Florent THOMAS-MOREL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
